4d9b3f3afba6e477c31d62f139fddf4dc27928d8
Oleksandr Brudnovskyi <oleksandr.brudnovskyi@customertimes.com>
2026-06-14 10:47:03 +0200

    Remove tracked artifacts from repo

--- dist/@mf-types/compiled-types/src/App.d.ts
+++
@@ -1,2 +0,0 @@
-import "./index.css";
-export default function App(): import("react").JSX.Element;
--- dist/assets/virtualExposes-BPTTW4xF.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./virtual_mf-exposes___mfe_internal__mines__remoteEntry_js-DllS3V11.js";export{e as default};
