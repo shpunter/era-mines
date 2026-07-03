0048e82697637532a7af048b31bd4cde921c9338
Oleksandr Brudnovskyi <oleksandr.brudnovskyi@customertimes.com>
2026-07-03 17:45:46 +0200

    add idb

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: ok
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
